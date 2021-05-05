d29519c1f9c6dc73936e98d70eee902299c5963e
endiawilliams <endiawilliams1@gmail.com>
2021-05-05 14:23:05 -0500

    Forgot to add routes in App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+)
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <HomePage />
+      <div>
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/hats' component={HatsPage} />
+        </Switch>
       </div>
     )
   }
